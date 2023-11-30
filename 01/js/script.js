f086502774a05f420191d9ae95c990c34d2986b7
birds0501 <birds0501@naver.com>
2023-11-30 13:21:57 +0900

    함수로 변경 완료

--- 01/js/script.js
+++ 01/js/script.js
@@ -4,25 +4,21 @@ $(function () {
   const $tabContent = $(".tab-con-item");
 
   //초기 세팅
-  tabAction();
+  tabAction(0);
 
   //탭메뉴 클릭했을 때
   $tabMenu.on("click", function () {
-    $tabMenu.removeClass("on");
-
     let idx = $(this).index();
-    $tabMenu.eq(idx).addClass("on");
 
-    $tabContent.hide();
-    $tabContent.eq(idx).show();
+    tabAction(idx);
   });
 
-  //중복되는 동작을 함수로 정의
-  function tabAction() {
+  //중복되는 동작을 함수로 정의(index는 매개변수이기 때문에 이름 변경 가능함. 다른 값들을 끌어오는 역할)
+  function tabAction(index) {
     $tabContent.hide();
-    $tabContent.eq(0).show();
+    $tabContent.eq(index).show();
 
     $tabMenu.removeClass("on");
-    $tabMenu.eq(0).addClass("on");
+    $tabMenu.eq(index).addClass("on");
   }
 });
